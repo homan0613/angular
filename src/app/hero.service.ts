0f6af5cb073f76fd60bd4f9e08b527f6579ba587
Ho Thi Man <manht@aureole.local>
2019-04-01 15:18:13 +0700

    the first

---
+++ src/app/hero.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {Hero} from './hero';
+import {HEROES} from './smoke-hero';
+import {Observable, of} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes():Observable<Hero[]>{
+    return of(HEROES);
+  }
+}
